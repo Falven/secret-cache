dd3231a50b8e9a60468df8f01237e573140d3234
Francisco Aguilera <falven2000@hotmail.com>
2020-08-23 16:08:23 -0400

    Initial commit.

---
+++ index.js
@@ -0,0 +1,52 @@
+/*
+ * Copyright (c) Microsoft Corporation. All rights reserved.
+ * Licensed under the MIT License. See License.txt in the project root for
+ * license information.
+ */
+
+'use strict';
+
+const { DefaultAzureCredential } = require("@azure/identity");
+const { SecretClient } = require("@azure/keyvault-secrets");
+const { EventGridClient } = require("@azure/eventgrid");
+
+/*
+ * Maintains a cache of secrets from the given Azure Key Vault and
+ * updates the cache when receiving an Event Grid notification.
+ */
+module.exports = class EventDrivenSecretCache {
+
+  #secretCache;
+  #secretClient;
+
+  /*
+  * Maintains a cache of secrets from the given Azure Key Vault and
+  * updates the cache when receiving an Event Grid notification.
+  */
+  constructor() {
+    this.#secretCache = [];
+  }
+
+  async init() {
+    // DefaultAzureCredential expects the following three environment variables:
+    // * AZURE_TENANT_ID: The tenant ID in Azure Active Directory
+    // * AZURE_CLIENT_ID: The application (client) ID registered in the AAD tenant
+    // * AZURE_CLIENT_SECRET: The client secret for the registered application
+    const credential = new DefaultAzureCredential();
+
+    // Build the URL to reach our key vault
+    const vaultName = process.env['AZURE_KEYVAULT_NAME'];
+    if(!vaultName)
+      throw new Error('EventDrivenSecretCache requires the Azure Key Vault Name to be present in the process environment variables.');
+    const url = `https://${vaultName}.vault.azure.net`;
+
+    // Lastly, create our secrets client and connect to the service
+    this.#secretClient = new SecretClient(url, credential);
+
+    const properties = this.#secretClient.listPropertiesOfSecrets();
+    for await (let secretProperty of properties) {
+      const secretName = secretProperty.name;
+      this.#secretCache[secretName] = await this.#secretClient.getSecret(secretName);
+    }
+  }
+}
